74a24dbb14f5b18e6234113377a828eaeeaf7de7
michaelsmith1994 <michaelsmith1994@yahoo.com>
2022-10-25 19:17:48 -0500

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ASSIG1_BinaryConverter.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href ="StyleSheet2.css" rel="stylesheet" />
+
+</head>
+<body>
+        
+    <form id="form1" runat="server" class="auto-style1">     
+            <div class="auto-style4">
+        &nbsp;
+        <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="XX-Large" Text="Numeric Conversion Tool" BackColor="Black" ForeColor="Lime" CssClass="default-row-spacer"></asp:Label>
+      
+                <br />
+      
+        <asp:Label ID="Label2" runat="server" Font-Bold="True" Text="From:" BackColor="White" CssClass="default-row-spacer2"></asp:Label>
+
+        <asp:DropDownList ID="numList1" runat="server" AutoPostBack="True" ValidationGroup="dropList" CssClass="default-row-spacer2" OnSelectedIndexChanged="numList1_SelectedIndexChanged">
+            <asp:ListItem>Select A Value</asp:ListItem>
+            <asp:ListItem>Decimal</asp:ListItem>
+            <asp:ListItem>Hexadecimal</asp:ListItem>
+            <asp:ListItem>Octal</asp:ListItem>
+            <asp:ListItem>Binary</asp:ListItem>
+        </asp:DropDownList>
+        <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="True" Height="21px" Width="147px" CssClass="default-row-spacer2"></asp:TextBox>
+                <br />
+                &nbsp;<asp:Label ID="Label3" runat="server" Font-Bold="True" Text="To:" BackColor="White" CssClass="default-row-spacer2"></asp:Label>
+                
+        <asp:DropDownList ID="numList2" runat="server" AutoPostBack="True" ValidationGroup="dropList" Width="147px" Height="21px" CssClass="default-row-spacer">
+        </asp:DropDownList>
+       
+            
+            <p class="auto-style5">
+            &nbsp;<asp:Label ID="Label5" runat="server" Font-Bold="True" Text="Converted Equivalent:" BackColor="White" CssClass="auto-style5"></asp:Label>
+
+            <asp:Label ID="result1" runat="server" BackColor="White" CssClass="auto-style4"></asp:Label>
+        </p>
+        <p class="auto-style5">
+            <asp:Button ID="Button1" runat="server" Text="Clear" ForeColor="#FF3300" OnClick="Button1_Click" CssClass="auto-style4" Height="26px" />
+<asp:Button ID="Button2" runat="server" Font-Bold="True" Text="Calculate" OnClick="Button2_Click" CssClass="auto-style4" />
+            
+        </p>
+        
+    </form>
+    <p>
+    </p>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASSIG1_BinaryConverter
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            
+            if (numList1.SelectedIndex == 1 && numList2.SelectedIndex == 1)
+            {
+                try
+                {
+                    int numberused = Convert.ToInt32(TextBox1.Text);
+                    string hexValue = numberused.ToString("X");
+                    result1.Text = hexValue;              
+
+                }
+                    
+                catch (System.Exception exception)
+                {
+                    result1.Text = exception.Message;   
+                }
+                
+
+            }
+            if (numList1.SelectedIndex == 1 && numList2.SelectedIndex == 2)
+            {
+                try
+                {
+                    int numberused = Convert.ToInt16(TextBox1.Text);
+                    string newoct = Convert.ToString(deciToOct(numberused));
+                    result1.Text = newoct;
+
+                }
+
+                catch (System.Exception exception)
+                {
+                    result1.Text = exception.Message;
+                }
+
+            }
+           
+            if (numList1.SelectedIndex == 1 && numList2.SelectedIndex == 3)
+            {
+                try
+                {
+                    int numberused = Convert.ToInt16(TextBox1.Text);
+                    string newbin = Convert.ToString(deciTobin(numberused));
+                    result1.Text = newbin;
+
+                }
+
+                catch (System.Exception exception)
+                {
+                    result1.Text = exception.Message;
+                }
+            }
+            if (numList1.SelectedIndex == 2 && numList2.SelectedIndex == 1)
+            {
+                try
+                {
+                    int decValue = int.Parse(TextBox1.Text, System.Globalization.NumberStyles.HexNumber);
+                    string newbin = Convert.ToString(decValue);
+                    result1.Text = newbin;
+
+                }
+
+                catch (System.Exception exception)
+                {
+                    result1.Text = exception.Message;
+                }
+            
+            }
+            if (numList1.SelectedIndex == 2 && numList2.SelectedIndex == 2)
+            {
+                try
+                {
+                    int decValue = int.Parse(TextBox1.Text, System.Globalization.NumberStyles.HexNumber);
+                    string newoct = Convert.ToString(deciToOct(decValue));
+                    result1.Text = newoct;
+
+                }
+
+                catch (System.Exception exception)
+                {
+                    result1.Text = exception.Message;
+                }
+            }
+            if (numList1.SelectedIndex == 2 && numList2.SelectedIndex == 3)
+            {
+                try
+                {
+                    int decValue = int.Parse(TextBox1.Text, System.Globalization.NumberStyles.HexNumber);
+                    string newbin = Convert.ToString(deciTobin(decValue));
+                    result1.Text = newbin;
+                }
+
+                catch (System.Exception exception)
+                {
+                    result1.Text = exception.Message;
+                }
+            }
+            if (numList1.SelectedIndex == 3 && numList2.SelectedIndex == 1)/*octal to decimal*/
+            {
+                int n = Convert.ToInt32(TextBox1.Text, 8);
+                result1.Text = Convert.ToString(n);
+
+            }
+            if (numList1.SelectedIndex == 3 && numList2.SelectedIndex == 2)/*octal to hex*/
+            {
+                int n = Convert.ToInt32(TextBox1.Text, 8);
+                string n2 = n.ToString("X");
+                result1.Text = n2;
+
+            }
+            if (numList1.SelectedIndex == 3 && numList2.SelectedIndex == 3)/*octal to binary*/
+            {
+                int n = Convert.ToInt32(TextBox1.Text, 8);
+                string newbin = Convert.ToString(deciTobin(n));
+                result1.Text = newbin;
+            }
+            if (numList1.SelectedIndex == 4 && numList2.SelectedIndex == 1)
+            {
+                try
+                {
+                    result1.Text = Convert.ToInt32(TextBox1.Text, 2).ToString();
+
+                }
+
+                catch (System.Exception exception)
+                {
+                    result1.Text = exception.Message;
+                }
+               
+            }
+            if (numList1.SelectedIndex == 4 && numList2.SelectedIndex == 2)
+            {
+                try
+                {
+                    String tempBinDec = Convert.ToInt32(TextBox1.Text, 2).ToString();
+                    int numberused = Convert.ToInt32(tempBinDec);
+                    string hexValue = numberused.ToString("X");
+                    result1.Text = hexValue;
+
+                }
+
+                catch (System.Exception exception)
+                {
+                    result1.Text = exception.Message;
+                }
+            }
+            if (numList1.SelectedIndex == 4 && numList2.SelectedIndex == 3)
+            {
+                try
+                {
+                    String tempBinDec = Convert.ToInt32(TextBox1.Text, 2).ToString();
+                    int numberused = Convert.ToInt16(tempBinDec);
+                    string newoct = Convert.ToString(deciToOct(numberused));
+                    result1.Text = newoct;
+
+                }
+
+                catch (System.Exception exception)
+                {
+                    result1.Text = exception.Message;
+                }
+            }
+
+        }
+        public string deciTobin(int n)
+        {
+            int quotient;
+            string rem = "";
+            while (n >= 1)
+            {
+                quotient = n / 2;
+                rem += (n % 2).ToString();
+                n = quotient;
+            }
+            string binary = "";
+            for (int i = rem.Length - 1; i >= 0; i--)
+            {
+                binary = binary + rem[i];
+            }
+            return binary;
+        }
+        public string deciToOct(int n)
+        {
+            int[] octalNumber = new int[100];
+            int quotient, i = 1, j;
+            string Octal = "";
+            quotient = n;
+            while (quotient != 0)
+            {
+                octalNumber[i++] = quotient % 8;
+                quotient = quotient / 8;
+            }        
+            for (j = i - 1; j > 0; j--)
+                Octal += octalNumber[j].ToString();
+            return Octal;
+        }
+       
+       
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+            result1.Text = "";
+            numList1.SelectedIndex = 0;
+            numList2.SelectedIndex = 0;
+        }
+
+        protected void numList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (numList1.SelectedIndex == 1)
+            {
+                numList2.Items.Clear();
+                numList2.Items.Add(new ListItem("Select a Value"));
+                numList2.Items.Add(new ListItem("Hexa-Decimal"));
+                numList2.Items.Add(new ListItem("Octal"));
+                numList2.Items.Add(new ListItem("Binary"));               
+            }
+
+            else if (numList1.SelectedIndex == 2)
+            {
+                numList2.Items.Clear();
+                numList2.Items.Add(new ListItem("Select a Value"));
+                numList2.Items.Add(new ListItem("Decimal"));
+                numList2.Items.Add(new ListItem("Octal"));
+                numList2.Items.Add(new ListItem("Binary"));
+
+            }
+            else if (numList1.SelectedIndex == 3)
+            {
+                numList2.Items.Clear();
+                numList2.Items.Add(new ListItem("Select a Value"));
+                numList2.Items.Add(new ListItem("Decimal"));
+                numList2.Items.Add(new ListItem("Hexa-Decimal"));
+                numList2.Items.Add(new ListItem("Binary"));
+            }
+            else if (numList1.SelectedIndex == 4)
+            {
+                numList2.Items.Clear();
+                numList2.Items.Add(new ListItem("Select a Value"));
+                numList2.Items.Add(new ListItem("Decimal"));
+                numList2.Items.Add(new ListItem("Hexa-Decimal"));
+                numList2.Items.Add(new ListItem("Octal"));
+            }
+            else if (numList1.SelectedIndex == 0)
+            {
+                numList2.Items.Clear();
+                numList2.Items.Add(new ListItem("Select a initial value to convert first!"));
+            }
+
+        }
+    }
+}
+    
